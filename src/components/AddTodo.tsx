ce67449bb8812325077754f977199a104d44889b
tobuya <thomasobuya95@gmail.com>
2023-07-21 11:36:10 +0300

    Add input form for adding todo string inputs

--- src/components/AddTodo.tsx
+++ src/components/AddTodo.tsx
@@ -1,8 +1,28 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { toast } from 'react-hot-toast'
-import { useTodo } from '../context/useTodo'
+import { useTodo } from '../context'
 import { Input } from './Input'
 
 export const AddTodo = () => {
-  return <div>Add To Do</div>
+  const [input, setInput] = useState<string>("")
+
+  return (
+    <form>
+      <div className="flex items-center w-full max-w-lg gap-2 p-5 m-auto">
+        <input
+          value={input}
+          onChange={e => setInput(e.target.value)}
+          type="text"
+          className="w-full px-5 py-2 bg-transparent border-2 outline-none border-zinc-600 rounded-xl placeholder:text-zinc-500 focus:border-white"
+          placeholder="Start typing ..."
+        />
+        <button
+          type="submit"
+          className="px-5 py-2 text-sm font-normal text-blue-300 bg-blue-900 border-2 border-blue-900 active:scale-95 rounded-xl"
+        >
+          Submit
+        </button>
+      </div>
+    </form>
+  )
 }
