ed154afb0a9b065fa60dd01e57dee4e44daf1236
tobuya <thomasobuya95@gmail.com>
2023-07-24 15:13:17 +0300

    Implement TodoContextProps, add new todo with useState and custom function, and provide updated context to child components

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -1,19 +1,29 @@
-import React, { createContext, useState } from 'react'
-import { nanoid } from 'nanoid'
-import { useLocalStorage } from 'usehooks-ts'
+import React, { createContext, useState } from 'react';
+import { nanoid } from 'nanoid';
+import { useLocalStorage } from 'usehooks-ts';
 
-export interface Todo {
-  id: string
-  text: string
-  status: 'undone' | 'completed'
+export interface TodoContextProps {
+  todos: string[];
+  addTodo: (text: string) => void;
 }
 
-export const TodoContext = createContext<undefined>(undefined)
+export const TodoContext = createContext<TodoContextProps|undefined>(undefined);
 
 export const TodoProvider = (props: { children: React.ReactNode }) => {
+  const [todos, setTodos] = useState<string[]>([])
+
+  const addTodo = (text: string) => {
+    setTodos([...todos, text]);
+  }
+
+  const value: TodoContextProps = {
+    todos,
+    addTodo,
+  };
+
   return (
-    <TodoContext.Provider value={undefined}>
+    <TodoContext.Provider value={value}>
       {props.children}
     </TodoContext.Provider>
   )
-}
+};
