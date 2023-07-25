fc5327707169bf76ba3fcc2fb6ee79ae7a51c187
tobuya <thomasobuya95@gmail.com>
2023-07-25 12:21:45 +0300

    Implement the Todo interface in the context

--- src/context/TodoContext.tsx
+++ src/context/TodoContext.tsx
@@ -2,19 +2,31 @@ import React, { createContext, useState } from 'react';
 import { nanoid } from 'nanoid';
 import { useLocalStorage } from 'usehooks-ts';
 
-export interface TodoContextProps {
-  todos: string[];
+interface TodoContextProps {
+  todos: Todo[];
   addTodo: (text: string) => void;
+}
+
+export interface Todo {
+  id: string;
+  text: string;
+  status: 'undone' | 'completed'
 }
 
 export const TodoContext = createContext<TodoContextProps|undefined>(undefined);
 
 export const TodoProvider = (props: { children: React.ReactNode }) => {
-  const [todos, setTodos] = useState<string[]>([])
+  const [todos, setTodos] = useState<Todo[]>([])
 
   const addTodo = (text: string) => {
-    setTodos([...todos, text]);
-  }
+    const newTodo: Todo = {
+      id: nanoid(),
+      text,
+      status: 'undone',
+    }
+
+    setTodos([...todos, newTodo]);
+  };
 
   const value: TodoContextProps = {
     todos,
